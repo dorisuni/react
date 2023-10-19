8a56fa30617ee96d4482384a18c5458be0207e6b
dorisuni <daizy13125@gmail.com>
2023-10-19 16:58:26 +0900

    새로운배열추가중 오류해결해야함

--- first/src/components/Expenses/ExpenseItem.js
+++ first/src/components/Expenses/ExpenseItem.js
@@ -7,10 +7,10 @@ function ExpenseItem(props) {
     
     return( 
     <Card className='expense-item'>
-       <ExpenseDate date={props.items.date} />
+       <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
-            <h2>{props.items.title}</h2>
-            <div className='expense-item__price'>${props.items.amount}</div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
         </div>
     </Card>
     );
--- first/src/components/Expenses/Expenses.js
+++ first/src/components/Expenses/Expenses.js
@@ -1,49 +1,41 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 
-function Expenses(props){
-    const [filteredYear,setFilteredYear] = useState('2020');
+function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
 
     let filterInfoText = '2019,2021,2022 & 2023'
 
-    if (filteredYear === '2019'){
-        filterInfoText=('2020,2021,2022 & 2023');
-        }else if((filteredYear === '2020')){
-            filterInfoText=('2019,2021,2022 & 2023');
-        }else if((filteredYear === '2021')){
-            filterInfoText=('2019,2020,2022 & 2023');
-        }else if((filteredYear === '2022')){
-            filterInfoText=('2019,2020,2021 & 2023');
-        }else{
-            filterInfoText=('2019,2020,2021 & 2022')
-        }
+    if (filteredYear === '2019') {
+        filterInfoText = ('2020,2021,2022 & 2023');
+    } else if ((filteredYear === '2020')) {
+        filterInfoText = ('2019,2021,2022 & 2023');
+    } else if ((filteredYear === '2021')) {
+        filterInfoText = ('2019,2020,2022 & 2023');
+    } else if ((filteredYear === '2022')) {
+        filterInfoText = ('2019,2020,2021 & 2023');
+    } else {
+        filterInfoText = ('2019,2020,2021 & 2022')
+    }
 
-    const filterChangeHandler = (selectedYear) =>{
+    const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
-        
+
     };
-    const items = props.items;
-    return(
+    return (
         <div>
-        <Card className='expenses'>
-            <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler} />
-            <p className='expenses-filter'>Data for years { filterInfoText }  is hidden.</p>
-                <ExpenseItem 
-            items = {items[0]}></ExpenseItem>
-                <ExpenseItem 
-            items = {items[1]}></ExpenseItem>
-                <ExpenseItem 
-            items = {items[2]}></ExpenseItem>
-                <ExpenseItem 
-            items = {items[3]}></ExpenseItem>
-        </Card>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <p className='expenses-filter'>Data for years {filterInfoText}  is hidden.</p>
+                {props.items.map((expense) => (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />))}
+            </Card>
         </div>
 
     );
-      
+
 }
 
 
