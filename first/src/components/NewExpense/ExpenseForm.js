794bd38348c2333ca85a3dd1792753927daaf49e
dorisuni <daizy13125@gmail.com>
2023-10-19 14:11:09 +0900

    자식-> 부모 컴퍼넌트로 정보 올리기

--- first/src/components/NewExpense/ExpenseForm.js
+++ first/src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import './ExpenseForm.css';
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle,setEnteredTitle] = useState('');
     const [enteredAmount,setEnteredAmount] = useState('');
     const [enteredDate,setEnteredDate] = useState('');
@@ -54,23 +54,34 @@ const ExpenseForm = () => {
     //         enteredDate : event.target.value
     //     })
     // }
-
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            Date: new Date(enteredDate)
+        };
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
 
     return (
 
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text' onChange={ titleChangeHandler } />
+                    <input type='text' value={enteredTitle} onChange={ titleChangeHandler } />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number' onChange={ amountChangeHandler } min='0.01' step="0.01" />
+                    <input type='number' value={enteredAmount} onChange={ amountChangeHandler } min='0.01' step="0.01" />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' onChange={ dateChangeHandler } min="2019-01-01" max="2023-12-31" />
+                    <input type='date' value={enteredDate} onChange={ dateChangeHandler } min="2019-01-01" max="2023-12-31" />
                 </div>
             </div>
             <div className="new-expense__action">
